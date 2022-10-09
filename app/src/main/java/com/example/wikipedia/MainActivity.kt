5ece9755c15eb84e9e14565cf667e93f65549f2a
Amir hossein <amirhossein.noghanii@gmail.com>
2022-10-09 17:12:20 +0330

    Set toolbar

--- app/src/main/java/com/example/wikipedia/MainActivity.kt
+++ app/src/main/java/com/example/wikipedia/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.ToolbarMain)
     }
 }
